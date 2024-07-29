905a0fc241f4ad374c9447ae41b47791ccbc952c
Hunter <hgouldthorpe95@gmail.com>
2024-07-28 20:37:55 -0400

    Added more responsiveness

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var headerH1 = document.getElementById('header-h1');
-    var headerH2 = document.getElementById('header-h2');
+document.addEventListener('DOMContentLoaded', () => {
+  const skills = document.querySelectorAll('.skill');
   
-    // Listen for the 'animationend' event on #header-h1
-    headerH1.addEventListener('animationend', function () {
-      // Once #header-h1 animation is complete, trigger #header-h2 animation
-      headerH2.style.animation = 'fadeInAndSlide 1s ease-in-out forwards'; // Apply the animation
-      headerH2.style.transition = 'opacity 1s ease-in-out'; // Add transition for a smooth fade effect
-      headerH2.style.opacity = '1';
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target); // Stop observing the visible element
+      }
     });
-  }); 
+  }, { threshold: 0.1 });
+  
+  skills.forEach(skill => {
+    observer.observe(skill);
+  });
+});
