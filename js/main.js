7af45ba43f8d0e3e01282003580d592f3cd39a41
Hunter <hgouldthorpe95@gmail.com>
2025-02-10 12:50:52 -0500

    Styling changes

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.nav-bar ul');
-  
-    toggleButton.addEventListener('click', () => {
-      menu.classList.toggle('active');
-    });
+  const toggleButton = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.nav-bar ul');
+
+  toggleButton.addEventListener('click', () => {
+    menu.classList.toggle('active');
   });
+});
 
-  function fadeInOnScroll() {
-    const elements = document.querySelectorAll('.fade-in');
+function fadeInOnScroll() {
+  const elements = document.querySelectorAll('.fade-in-skills');
 
-    elements.forEach(element => {
-      const elementTop = element.getBoundingClientRect().top;
-      const windowHeight = window.innerHeight;
+  elements.forEach(element => {
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
 
-      if (elementTop < windowHeight) {
-        element.classList.add('visible');
-      }
-    })
-  }
+    if (elementTop < windowHeight) {
+      element.classList.add('visible');
+    }
+  });
+}
 
-  window.addEventListener('scroll', fadeInOnScroll);
+function fadeInProjectScroll() {
+  const elements = document.querySelectorAll('.fade-in');
+
+  elements.forEach(element => {
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (elementTop < windowHeight) {
+      element.classList.add('visible');
+    }
+  });
+}
+
+window.addEventListener('scroll', fadeInOnScroll);
+window.addEventListener('scroll', fadeInProjectScroll);
